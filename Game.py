4c83ee88aa591af5e36d91da74be0224f93c16cd
Daria Kowalewa <kowalewa.daria2012@gmail.com>
2023-05-11 15:01:10 +0300

    add files

---
+++ Game.py
@@ -0,0 +1,38 @@
+import random
+words = ['python', 'programming', 'language', 'computer', 'disk', 'dock', 'data',]
+word = random.choice(words)
+guessed = []
+max_attempts = 6
+
+def display_word(word, guessed):
+    for letter in word:
+        if letter in guessed:
+            print(letter, end=' ')
+        else:
+            print('_', end=' ')
+    print()
+
+def is_word_guessed(word, guessed):
+    for letter in word:
+        if letter not in guessed:
+            return False
+    return True
+
+
+while max_attempts > 0:
+    print(f'У вас {max_attempts} попыток.')
+    display_word(word, guessed)
+    guess = input('Введите букву: ').lower()
+    if guess in guessed:
+        print('Вы уже угадали эту букву!')
+    elif guess in word:
+        guessed.append(guess)
+        if is_word_guessed(word, guessed):
+            print(f'Вы победили! Загаданное слово было "{word}".')
+            break
+    else:
+        print('Такой буквы нет в загаданном слове.')
+        max_attempts -= 1
+else:
+    print(f'Вы проиграли. Загаданное слово было "{word}".')
+
